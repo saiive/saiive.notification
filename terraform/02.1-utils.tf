9d4210b78a291242d45961518a54caeba82cfcdc
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2021-05-18 14:15:41 +0200

    change service subscription and add dns

--- terraform/02.1-utils.tf
+++ terraform/02.1-utils.tf
@@ -2,7 +2,7 @@ data "azurerm_client_config" "current" {}
 
 
 resource "azurerm_key_vault" "keyvault" {
-  name                        = "${var.environment}-${var.prefix}-vault"
+  name                        = "${var.environment}-${var.prefix}"
   location                    = var.location
   resource_group_name         = azurerm_resource_group.rg.name
   enabled_for_disk_encryption = true
